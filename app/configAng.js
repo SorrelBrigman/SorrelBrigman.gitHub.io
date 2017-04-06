4cbb2651b209765d04f128c1d0422254e31fd53b
Sorrel Brigman <sorrel.k@gmail.com>
2017-04-06 11:30:18 -0500

    broke site into angular components

---
+++ app/configAng.js
@@ -0,0 +1,38 @@
+//assign the angular module to the var app
+var app = angular.module("myApp", ['ngRoute']);
+
+//configure "app" with routeProvider
+app.config(($routeProvider)=> {
+  $routeProvider
+    //when at the base page
+    .when("/", {
+      //use the Home Ctrl
+      controller: "homeCtrl",
+      //use the partial "home"
+      templateUrl : "partials/home.html"
+    })
+    .when("/about", {
+      // use the about ctrl
+      controller: "aboutCtrl",
+      //use the partial "addProduct"
+      templateUrl: "partials/about.html"
+    })
+    .when("/connect", {
+      // use the connect ctrl
+      controller: "connectCtrl",
+      //use the partial "connect"
+      templateUrl: "partials/connect.html"
+    })
+    .when("/projects", {
+      // use the projects ctrl
+      controller: "projectsCtrl",
+      //use the partial "projects"
+      templateUrl: "partials/inReview.html"
+    })
+    .when("/technologies", {
+      // use the technologies ctrl
+      controller: "technologiesCtrl",
+      //use the partial "technologies"
+      templateUrl: "partials/technologies.html"
+    })
+});
---
+++ app/controllers/technologiesCtrl.js
@@ -0,0 +1,10 @@
+app.controller('technologiesCtrl', function(){
+
+//find any nav links with the active class
+$('a.active').removeClass("active");
+//remove active class
+//apply actice class to about nav link
+$("a.about").addClass("active");
+
+
+});
